fad116578d569233ebfbb8c0f3dbc4b097e1c1f6
Jordan Kelly <profjjk@gmail.com>
2021-02-17 14:17:16 -0800

    create api routes, but missing query params

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,53 @@
-const db = require("./models");
+const db = require("../models");
 
 module.exports = function(app) {
-  // Post new exercise to current workout.
+  // Get last workout.
+  app.get('api/workouts', (req, res) => {
+    db.Workout.find({}) // Missing query parameters
+      .then(dbWorkout => {
+        console.log(dbWorkout)
+        res.json(dbWorkout)
+      })
+      .catch(err => {
+        console.log(err)
+        res.json(err);
+      });
+  })
+  // Get last 7 workouts.
+  app.get('api/workouts/range', (req, res) => {
+    db.Workout.find({}) // Missing query parameters
+      .then(dbWorkout => {
+        console.log(dbWorkout)
+        res.json(dbWorkout)
+      })
+      .catch(err => {
+        console.log(err)
+        res.json(err);
+      });
+  })
+  // Create a new workout.
   app.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        console.log(dbWorkout)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+  // Modify current workout with new exercise.
+  app.put('/api/workouts/:id', ({ body }, res) => {
+    // How do I get the id from the URL using { body } ?
+    console.log(body)
     db.Exercise.create(body)
-      .then(({ _id }) => db.Workout.findOneAndUpdate({}, 
+      .then(({ _id }) => db.Workout.findOneAndUpdate({}, // Missing query parameters
         { $push: { exercises: _id }}, { new: true }))
       .then(dbWorkout => {
+        console.log(dbWorkout)
         res.json(dbWorkout);
       })
       .catch(err => {
+        console.log(err)
         res.json(err)
       });
   });
