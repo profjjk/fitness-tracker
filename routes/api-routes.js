8ce1ef3f8f01c5e8f50dc26fc5405d0aab07b0d0
Jordan Kelly <profjjk@gmail.com>
2021-02-17 14:24:18 -0800

    add console logs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,8 @@ const db = require("../models");
 module.exports = function(app) {
   // Get last workout.
   app.get('api/workouts', (req, res) => {
+    console.log("Request: ")
+    console.log(req)
     db.Workout.find({}) // Missing query parameters
       .then(dbWorkout => {
         console.log(dbWorkout)
@@ -15,6 +17,8 @@ module.exports = function(app) {
   })
   // Get last 7 workouts.
   app.get('api/workouts/range', (req, res) => {
+    console.log("Request: ")
+    console.log(req)
     db.Workout.find({}) // Missing query parameters
       .then(dbWorkout => {
         console.log(dbWorkout)
@@ -27,6 +31,8 @@ module.exports = function(app) {
   })
   // Create a new workout.
   app.post('/api/workouts', ({ body }, res) => {
+    console.log("Body: ")
+    console.log(body)
     db.Workout.create(body)
       .then(dbWorkout => {
         console.log(dbWorkout)
@@ -38,6 +44,7 @@ module.exports = function(app) {
   // Modify current workout with new exercise.
   app.put('/api/workouts/:id', ({ body }, res) => {
     // How do I get the id from the URL using { body } ?
+    console.log("Body: ")
     console.log(body)
     db.Exercise.create(body)
       .then(({ _id }) => db.Workout.findOneAndUpdate({}, // Missing query parameters
