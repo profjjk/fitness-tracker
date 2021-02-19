dbaa9b3950cc54273db739305ba471ac84782d43
Jordan Kelly <profjjk@gmail.com>
2021-02-18 17:57:42 -0800

    fix ID problem

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,65 +7,66 @@ const db = require("../models/");
 // ====================================================
 module.exports = function(app) {
   // Get last workout.
-  app.get('api/workouts', (req, res) => {
-    console.log(req)
-    db.Workout.findOne({})
-      .then(dbWorkout => {
-        console.log("--- Last Workout ---")
-        console.log(dbWorkout)
-        res.json(dbWorkout)
+  app.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).sort({ day: -1 }).limit(1)
+      .then(lastWorkout => {
+        console.log("--- Last Workout ---");
+        console.log(lastWorkout);
+        res.json(lastWorkout);
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
         res.json(err);
       });
   })
 
   // Get last 7 workouts.
-  app.get('api/workouts/range', (req, res) => {
-    db.Workout.find({}).limit(7)
-      .then(dbWorkout => {
-        console.log("--- Last 7 Workouts ---")
-        console.log(dbWorkout)
-        res.json(dbWorkout)
+  app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({}).sort({ day: -1 }).limit(7)
+      .then(weekWorkouts => {
+        console.log("--- Last 7 Workouts ---");
+        console.log(weekWorkouts);
+        res.json(weekWorkouts);
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
         res.json(err);
       });
   })
 
   // Create a new workout.
-  app.post('/api/workouts', ({ body }, res) => {
-    db.Workout.create(body)
-      .then(dbWorkout => {
-        console.log("--- New Workout ---")
-        console.log(dbWorkout)
+  app.post('/api/workouts', (req, res) => {
+    db.Workout.create(req.body)
+      .then(newWorkout => {
+        console.log("--- New Workout ---");
+        console.log(newWorkout);
+        res.json({_id: newWorkout._id});
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       })
   })
 
   // Modify current workout with new exercise.
-  app.put('/api/workouts/:id', ({ body }, res) => {
+  app.put('/api/workouts/:id', (req, res) => {
     console.log('--- Add Exercise ---')
-    console.log(body);
-    db.Workout.findOneAndUpdate({}, // How do I get the id of the current workout?
-      { $push: { exercises: body }}, { new: true })
-      .then(dbWorkout => {
-        console.log('--- Updated Workout ---')
-        console.log(dbWorkout)
-        res.json(dbWorkout);
+    console.log(req.body);
+    db.Workout.findOneAndUpdate({_id: req.params.id},
+      { $push: { exercises: req.body }}, { new: true })
+      .then(updWorkout => {
+        console.log('--- Updated Workout ---');
+        console.log(updWorkout);
+        res.json(updWorkout);
       })
       .catch(err => {
-        console.log(err)
-        res.json(err)
+        console.log(err);
+        res.json(err);
       });
   });
 }
 
 // PROBLEMS
 // =================================================
-// 1. PUT route: adds exercise to wrong workout. Need to target correct workout by ID. How?
+// 1. PUT route adds exercise to wrong workout. Need to target most recent workout by ID. How?
 // 2. GET routes do not work at all. Why?
+// 3. WTF is going on in the api.js file?!
